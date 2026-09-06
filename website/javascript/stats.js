f3ad7b4159e2312b55a63cdfffc742b05eb4ea53
Crapcraft <admin@crapcraft.nz>
2026-09-06 23:38:52 +1200

    fixing stats.js

--- website/javascript/stats.js
+++ website/javascript/stats.js
@@ -10,9 +10,6 @@ fetch("./stats.json")
       document.getElementById("prowlarr-version").textContent = data.ProwlarrVersion;
       document.getElementById("radarr-version").textContent = data.RadarrVersion;
       document.getElementById("sonarr-version").textContent = data.SonarrVersion;
-      document.getElementById("lidarr-version").textContent = data.LidarrVersion;
-      document.getElementById("bazarr-version").textContent = data.BazarrVersion;
-      document.getElementById("openwebui-version").textContent = data.OpenwebuiVersion;
       document.getElementById("qbittorrent-version").textContent = data.QbittorrentVersion;
       document.getElementById("immich-photos").textContent = data.Photos;
       document.getElementById("immich-videos").textContent = data.Videos;
